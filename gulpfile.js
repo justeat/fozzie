af37f248e84b7fbf6f5f87cd7818a68ba887045d
ashleynolan <nolly00@gmail.com>
2017-05-09 16:19:59 +0100

    Updating css tasks to hook into gulp-build-fozzie and line ending joy.  Also moving eslint package inits

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,28 @@
   ===========
   Rather than manage one giant configuration file responsible
   for creating multiple tasks, each task has been broken out into
-  its own file in _build/tasks. Any files in that directory get
-  automatically required below.
+  its own file.
 
-  To add a new task, simply add a new task file that directory.
-  _build/tasks/default.js specifies the default set of tasks to run
-  when you run `gulp`.
+  So we can share common gulp tasks across projects these are
+  pulled in as an NPM module:
+  'gulp-build-fozzie'
+  => https://github.com/justeat/gulp-build-fozzie
+
+  Any tasks created in that module get automatically required below.
+
+  To add a new task, update gulp-build-fozzie, create a PR, and
+  push a new release.
 */
 
 var requireDir = require('require-dir');
 const build = require('@justeat/gulp-build-fozzie');
+const gulp = require('gulp');
+
 
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./_build/tasks', { recurse: true });
 
-build({
+gulp.tasks = build({
   js: {
     srcFile: 'fozzie.js'
   }
