26145721286c99c965d97c0151adccd37cc907bb
sonnyprince <sonny.prince.sp@gmail.com>
2018-09-25 11:11:39 +0100

    v0.100.0 - restructure tests, add body class test

--- src/js/modules/stopFoit/test/index.test.js
+++ src/js/modules/stopFoit/test/index.test.js
@@ -5,66 +5,82 @@ import { stopFoit } from '../index';
 jest.mock('fontfaceobserver');
 
 describe('module stopFoit', () => {
-    beforeAll(() => {
-        FontFaceObserver.mockImplementation(() => ({
-            load: () => new Promise(resolve => {
-                resolve();
-            })
-        }));
+    describe('when the fonts load correctly', () => {
+        beforeAll(() => {
+            FontFaceObserver.mockImplementation(() => ({
+                load: () => new Promise(resolve => {
+                    resolve();
+                })
+            }));
+        });
+
+        it('adds `has-fontsLoaded--headings` to the body when Ubunto webfont has loaded', () => {
+            // Arrange
+            TestUtils.setBodyHtml(`
+                <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+            `);
+
+            // Act
+            stopFoit();
+
+            // Assert
+            setTimeout(() => {
+                expect(FontFaceObserver).toBeCalled();
+                expect(document.body.classList.contains('has-fontsLoaded--headings')).toBe(true);
+            }, 100);
+        });
+
+        it('adds `has-fontsLoaded--base` to the body when Hind Vadodara webfont has loaded', async () => {
+            // Arrange
+            TestUtils.setBodyHtml(`
+                <link href="https://fonts.googleapis.com/css?family=Hind+Vadodara" rel="stylesheet">
+            `);
+
+            // Act
+            stopFoit();
+
+            // Assert
+            setTimeout(() => {
+                expect(FontFaceObserver).toBeCalled();
+                expect(document.body.classList.contains('has-fontsLoaded--base')).toBe(true);
+            }, 100);
+        });
     });
 
-    it('adds `has-fontsLoaded--headings` to the body when Ubunto webfont has loaded', () => {
-        // Arrange
-        TestUtils.setBodyHtml(`
-            <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-        `);
+    describe('when the fonts do not load correctly', () => {
+        beforeAll(() => {
+            FontFaceObserver.mockImplementation(() => ({
+                load: () => new Promise((...reject) => {
+                    reject();
+                })
+            }));
+        });
 
-        // Act
-        stopFoit();
+        it('causes FontFaceObserver to reject', () => {
+            // Arrange
+            TestUtils.setBodyHtml('');
 
-        // Assert
-        setTimeout(() => {
-            expect(FontFaceObserver).toBeCalled();
-            expect(document.body.classList.contains('has-fontsLoaded--headings')).toBe(true);
-        }, 100);
-    });
+            // Act
+            stopFoit();
 
-    it('adds `has-fontsLoaded--base` to the body when Hind Vadodara webfont has loaded', async () => {
-        // Arrange
-        TestUtils.setBodyHtml(`
-            <link href="https://fonts.googleapis.com/css?family=Hind+Vadodara" rel="stylesheet">
-        `);
+            // Assert
+            expect(FontFaceObserver.rejects);
+        });
 
-        // Act
-        stopFoit();
+        it('does not add any font loaded classes to the body', () => {
+            // Arrange
+            TestUtils.setBodyHtml(`
+                <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+            `);
 
-        // Assert
-        setTimeout(() => {
-            expect(FontFaceObserver).toBeCalled();
-            expect(document.body.classList.contains('has-fontsLoaded--base')).toBe(true);
-        }, 100);
-    });
-});
+            // Act
+            stopFoit();
 
-describe('module stopFoit catches Errors', () => {
-    beforeAll(() => {
-        FontFaceObserver.mockImplementation(() => ({
-            load: () => new Promise((...reject) => {
-                reject();
-            })
-        }));
-    });
-
-    it('rejects if fonts do not load', () => {
-        // Arrange
-        TestUtils.setBodyHtml(`
-            <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-        `);
-
-        // Act
-        stopFoit();
-
-        // Assert
-        expect(FontFaceObserver.rejects);
+            // Assert
+            setTimeout(() => {
+                expect(document.body.classList.contains('has-fontsLoaded--base')).toBe(false);
+                expect(document.body.classList.contains('has-fontsLoaded--headings')).toBe(false);
+            }, 100);
+        });
     });
 });
