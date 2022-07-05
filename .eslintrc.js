46746d0c1b99f0ae4b7aab3d30e9c2807303e54f
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-07-05 14:20:12 +0100

    remove eslint override

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     extends: '@justeat/eslint-config-fozzie',
     rules: {
-        'import/no-extraneous-dependencies': 'off',
         'import/prefer-default-export': 'off'
     }
 };
